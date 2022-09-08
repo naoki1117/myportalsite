05dcbbe65bd7b36f8c2da21f2ecbf9e7836e571c
naoki1117 <105569021+naoki1117@users.noreply.github.com>
2022-09-08 12:53:11 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  const countButton = () => {
+    setCount(count + 1);
+  };
+  const countDown = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h1>カウンター</h1>
+      <p>{count}</p>
+      <button onClick={countButton}>カウントアップ</button>
+      <br />
+      <button onClick={countDown}>カウントダウン</button>
+    </div>
+  );
+}
